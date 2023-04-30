d4246ecd2732a0d0c0798b7db5a4f26c7b85f4c4
Daniel <daniel@syncrew.com>
2023-04-30 20:38:47 +0000

    Chapter complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
+  const name='StarFuckers'
   return (
+    <div className="container">
+      <article>
     <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <img src="images/group.svg" alt="Stargazers Group" />
+      <h1>Meet the <i style={{color:"SteelBlue"}}>{name}</i></h1>
+      <p>Members of an <b>intergalactic alliance</b> <br />
+      Paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+    <button className="outline" onClick={()=>alert('Hi There')}>Click Me</button>
     </hgroup>
+    </article>
+    </div>
   )
 }
 export default App

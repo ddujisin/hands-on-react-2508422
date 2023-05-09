99a71cc2735a9fc77aef425b49b1f3a94e489750
Daniel <daniel@syncrew.com>
2023-05-09 22:00:53 +0000

    chapter

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,4 +1,4 @@
-export default ({ member, handleClose }) => {
+export default ({ member, handleClose, handleChange }) => {
   return (
     <dialog id="modal-member" open>
       <article>
@@ -19,6 +19,14 @@ export default ({ member, handleClose }) => {
             <hgroup>
               <h1>{member.name}</h1>
               <p>{member.bio}</p>
+              <hgroup>
+                <a href="#" className="outline" role ="button"
+                onClick={()=>{handleChange(Number(member.id)-1)}}
+                >previous</a>
+                <a href="#" className="outline" role ="button"
+                onClick={()=>{handleChange(Number(member.id)+1)}}
+                >next</a>
+                </hgroup>
             </hgroup>
           </div>
         </hgroup>
